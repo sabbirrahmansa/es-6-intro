b595ac2294a986c0526956efe089681aedbd4418
Md.Sabbir Rahman Saun <sabbirrahman3586@gmail.com>
2024-01-04 21:51:15 +0600

    javascript add

---
+++ backtik.js
@@ -0,0 +1,6 @@
+ const num1=30;
+ const num2= 40;
+
+ const math = `the sum of ${num1} and ${num2} is ${num1 + num2}`
+ console.log(math);
+ 
---
+++ defult.js
@@ -0,0 +1,8 @@
+function add(num1=0 , num2=0){
+    let result = 0;
+    result = num1+num2 ;
+    console.log(result);
+    
+}
+
+const sum = add(10,3);
